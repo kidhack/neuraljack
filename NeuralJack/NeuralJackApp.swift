2e5b81362ef36b02dd0ff1dc66cb14b046c58565
kidhack <kidhack@users.noreply.github.com>
2026-03-03 11:52:35 -0800

    Initial Commit

---
+++ NeuralJack/NeuralJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuralJackApp.swift
+//  NeuralJack
+//
+//  Created by kidhack on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NeuralJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
